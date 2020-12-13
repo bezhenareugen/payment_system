b869806ae76178d93fa28014f8aef63472f4a7a9
bezhenareugen <eugenbezhenar@gmail.com>
2020-12-14 00:33:56 +0300

    Added Exchangerate page with current currency value

---
+++ PaymentSystem/Server/Controllers/ExchangeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaymentSystem.Server.Data;
+using PaymentSystem.Server.Models;
+
+namespace PaymentSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ExchangeController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExchangeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<PrivateBankCurrencyApi> GetCurrenciesApi()
+        {
+            var currencyList = _context.PrivateBankCurrencies.ToList();
+
+            var skipedCurrencylist = currencyList.Skip(currencyList.Count - 4).ToList();
+
+            return skipedCurrencylist;
+
+
+        }
+    }
+}
---
+++ PaymentSystem/Shared/PrivateBankCurrencyApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentSystem.Shared
+{
+   public  class PrivateBankCurrencyApi
+    {
+        public int Id { get; set; }
+        public string Ccy { get; set; }
+        public string Base_Ccy { get; set; }
+        public decimal Buy { get; set; }
+        public decimal Sale { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
